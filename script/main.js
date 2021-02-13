fd4c10809cf9aca6534eed5f165d9dc3807c7bc2
terekhof <glebterekhof@gmail.com>
2021-02-13 21:31:56 +0400

    refactoring

--- script/main.js
+++ script/main.js
@@ -1,37 +1,21 @@
-const actionType = document.querySelector('#type');
 const button = document.querySelector("#button");
 
-
 button.addEventListener('click', function(){
-  if (actionType.value === "income") {
-    addAmount()
-  } else if (actionType.value === "expenses") {
-    subtractAmount();
-  } else {
-    addSavings();
-  }
-})
- 
-
-function addAmount() {
   let totalBudget = parseInt(document.querySelector('#budget').innerHTML);
-  let currentAmount = parseInt(document.querySelector("#amount").value);
-  totalBudget += currentAmount;
-  document.querySelector('#budget').innerHTML = totalBudget;
-}
-
-
-function subtractAmount() {
-  let totalBudget = parseInt(document.querySelector('#budget').innerHTML);
-  let currentAmount = parseInt(document.querySelector("#amount").value);
-  totalBudget -= currentAmount;
-  document.querySelector('#budget').innerHTML = totalBudget;
-}
-
-
-function addSavings() {
   let totalSavings = parseInt(document.querySelector('#savings').innerHTML);
   let currentAmount = parseInt(document.querySelector("#amount").value);
-  totalSavings += currentAmount;
-  document.querySelector('#savings').innerHTML = totalSavings;
-}
+  let actionType = document.querySelector('#type');
+  switch(actionType.value) {
+    case "income":
+      totalBudget += currentAmount;
+      document.querySelector('#budget').innerHTML = totalBudget;
+      break;
+    case "expenses":
+      totalBudget -= currentAmount;
+      document.querySelector('#budget').innerHTML = totalBudget;
+      break;
+    default:
+      totalSavings += currentAmount;
+      document.querySelector('#savings').innerHTML = totalSavings;
+  }
+})
