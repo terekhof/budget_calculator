9415fb68c9c0fb30133d593ad260bddac2eb4421
terekhof <glebterekhof@gmail.com>
2021-02-20 23:09:53 +0400

    added sort by feature

--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
 const button = document.querySelector("#button");
 const logTable = document.querySelector('.table-element');
 const buttonCleanAll = document.querySelector("#cleanAll");
+const tBody = document.querySelector("#tableContent");
+const selection = document.querySelector("#selectInput");
 
 const INCOME = "income";
 const EXPENSES = "expenses";
@@ -75,11 +77,20 @@ function appendData() {
     let tdAmount = document.createElement('td');
     tdAmount.innerText = log.amount;
 
+    if (tdCase.innerText === "Income") {
+      tr.setAttribute("data-status","income");
+    } else if (tdCase.innerText === "Expenses") {
+      tr.setAttribute("data-status","expenses");
+    } else {
+      tr.setAttribute("data-status","savings");
+    }
+
+    tBody.appendChild(tr);
     tr.appendChild(tdCase);
     tr.appendChild(tdDescription);
     tr.appendChild(tdAmount);
 
-    logTable.appendChild(tr);
+    logTable.appendChild(tBody);
     localStorage.setItem("logs", JSON.stringify(arrayData));
 
     setColorType(tdCase.innerText);
@@ -99,6 +110,35 @@ function setColorType(type) {
   }  
 }
 
-buttonCleanAll.addEventListener('click', function(){
-  arrayData = new Array();
+function sortData(selectedResult){
+  let logs = logTable.rows;
+  let logsHeader = logTable.rows[0]
+  
+  for (log of logs) {
+    logsHeader.style.display = "table-row";
+    log.style.display = "none";
+    if (log.innerHTML.includes("Income") && selectedResult === "Income") {
+      log.style.display = "table-row"; 
+    } else if (log.innerHTML.includes("Expenses") && selectedResult === "Expenses") {
+      log.style.display = "table-row"; 
+    } else if (log.innerHTML.includes("Savings") && selectedResult === "Savings") {
+      log.style.display = "table-row";
+    } else if (selectedResult === "All") {
+      log.style.display = "table-row";
+    }
+  }
+}
+
+selection.addEventListener('change', () => {
+  selectedResult = selection.options[selection.selectedIndex].text;
+  console.log(selectedResult);
+  sortData(selectedResult);
 })
+
+
+buttonCleanAll.addEventListener('click', function(){
+  arrayData = [];
+  tBody.innerHTML = "";
+})
+
+
